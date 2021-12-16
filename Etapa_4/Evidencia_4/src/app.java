9f55fa1d9ed3788bb6dffd3378783b2baefd25f6
Mario Gaona <gaonamario00@gmail.com>
2021-12-15 22:29:28 -0600

    Evidencia_4 Completa

--- Etapa_4/Evidencia_4/src/app.java
+++ Etapa_4/Evidencia_4/src/app.java
@@ -1,96 +1,158 @@
-import java.util.Scanner;
+import java.util.ArrayList; // Libreria necesaria para crear ArrayList
+import java.util.Scanner; // Librearia para crear objetos tipos Scanner
 
 public class app {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		// Se declaran e inicializan los arreglos de objetos
-		Empleado emp[] = new Empleado[100];
-		Programador pro[] = new Programador[100];
-		
 		// Se crea un objeto tipo scanner para los input
 		Scanner input = new Scanner(System.in);	
 		
 		// se declaran e inicializan las variables necesarias
-		boolean sn = true;
-		int contEmp = 0;
-		int contPro = 0;
+		boolean sn = true; // variable centinela para el ciclo while
+		int contEmp = 0;  // Contador de empleados
+		int contPro = 0; // Contador de programadores
+		
+		// ArrayList tipo empleado, el cual contendre objetos de tipo empleado y programador
+		// Como programador extiende de empleado el arrayList no tiene problema con guardar objetos tipo programador
+		ArrayList <Empleado> empleados = new ArrayList<Empleado>();
 		
 		// ciclo que se repite las veces que el usuario desee
 		while(sn) {
 			
-				System.out.println("\n1.- Empleado\n2.- Programador\n Ingrese cualquier otro caracter para salir");
+				// Pregunta si el usuario quere ingresar un empleado normal o programador
+				System.out.println("\n1.- Empleado normal\n2.- Programador\n Ingrese cualquier otro caracter para salir");
 				System.out.print("Que desea capturar: ");
 				String type = input.nextLine();
-				
+				System.out.print("\n");
 				switch(type) {
 				
-				case "1":
-					Empleado e = new Empleado();
-					System.out.println("Empleado #"+(contEmp+1));
-					System.out.print("Ingrese el nombre: ");
-					String nombre = input.nextLine();
-					e.setNombre(nombre);
+				case "1": // Caso para empleados normales
+					Empleado e = new Empleado(); //Crea objeto tipo empleado
 					
+					System.out.println("Empleado #"+(contEmp+1));
+					
+					// Pide el nombre o los nombres del empleado
+					System.out.print("Ingrese su nombre(s): ");
+					String nombre = input.nextLine();
+					
+					// Pide los apellidos del empleado
+					System.out.print("Ingrese sus apellidos: ");
+					String apellidos = input.nextLine();
+					
+					// Almacena el nombre completo de el empleado en el objeto
+					e.setNombre(nombre+" "+apellidos);
+					
+					// Pide la cedula del empleado
 					System.out.print("Ingrese la cedula: ");
 					String cedula = input.nextLine();
-					e.setCedula(cedula);
+					e.setCedula(cedula); // Almacena la cedula de el empleado
 					
+					// Pide la edad del empleado
 					System.out.print("Ingrese la edad: ");
-					int edad = input.nextInt(); input.nextLine();
+					int edad = input.nextInt();
+					
+					// while que validad que la edad sea de 18 hasta 45
+					while(edad<18 || edad>45) {
+						// Pide la edad del empleado de nuevo
+						System.out.print("Ingrese la edad (entre 18 y 45 años): ");
+						edad = input.nextInt();
+					}
+					input.nextLine();
+					
+					// Almacena la edad del empleado en el objeto
 					e.setEdad(edad); 
 					
+					// Pide el estado civil de el empleado
 					System.out.print("Ingrese estado civil: ");
 					String estadoCivil = input.nextLine();
+					
+					// Almacena el estado civil de el empleado en el objeto
 					e.setEstadoCivil(estadoCivil);
 					
+					// Pide el salario del empleado
 					System.out.print("Ingrese salario: ");
 					double salario = input.nextDouble(); input.nextLine();
-					e.setSalario(salario);
+					e.setSalario(salario); // almacena el salario
 					
-					emp[contEmp] = e;
+					// Agrega el objeto "e" al final del ArrayList empleados
+					empleados.add(e);
 					
+					// Aumenta contador de empleados
 					contEmp++;
 					
 					break;
 					
-				case "2":
+				case "2": // Caso para programadores
 					
+					// Crea un objeto tipo programador
 					Programador p = new Programador();
 					
 					System.out.println("Programador #"+(contEmp+1));
-					System.out.print("Ingrese el nombre: ");
-					String nombreP = input.nextLine();
-					p.setNombre(nombreP);
 					
+					// Pide el nombre o los nombres del programador
+					System.out.print("Ingrese su nombre(s): ");
+					String nombreP = input.nextLine();
+					
+					// Pide los apellidos del programador
+					System.out.print("Ingrese sus apellidos: ");
+					String apellidosP = input.nextLine();
+					
+					// almacena el nombre completo del programador en el objeto "p"
+					p.setNombre(nombreP+" "+apellidosP);
+					
+					// Pide la cedula del programador
 					System.out.print("Ingrese la cedula: ");
 					String cedulaP = input.nextLine();
-					p.setCedula(cedulaP);
+					p.setCedula(cedulaP); // Almacena la cedula en el objeto
 					
+					// Pide la edad del programador
 					System.out.print("Ingrese la edad: ");
-					int edadP = input.nextInt(); input.nextLine();
+					int edadP = input.nextInt();
+					
+					//While para validar que la edad sea de 18 hasta 45
+					while(edadP<18 || edadP>45) {
+						// Pide la edad del programador de nuevo
+						System.out.print("Ingrese la edad (entre 18 y 45 años): ");
+						edadP = input.nextInt();
+					}
+					input.nextLine();
+					
+					// Almacena la edad en el objeto
 					p.setEdad(edadP); 
 					
+					// Pide el estado civil de el programador
 					System.out.print("Ingrese estado civil: ");
 					String estadoCivilP = input.nextLine();
+					// Almacena el estado civil del programador en el objeto
 					p.setEstadoCivil(estadoCivilP);
 					
+					// Pide el salario del programador
 					System.out.print("Ingrese salario: ");
 					double salarioP = input.nextDouble(); input.nextLine();
+					
+					// Almacena el salario del programador en el objeto
 					p.setSalario(salarioP);
 					
+					// Pide el numero de lineas de codigo por hora 
 					System.out.print("Ingrese lineas de codigo por hora: ");
 					int lineasDeCodigo = input.nextInt(); input.nextLine();
+					
+					// Almacena las lineas de codigo en el objeto
 					p.setLineasDeCodigoPorHora(lineasDeCodigo);
 					
-					
+					// Pide el lenguaje dominante del programador
 					System.out.print("Ingrese lenguaje dominante: ");
 					String lenguajeDominante = input.nextLine();
+					
+					// Almacena el lenguaje dominante del programador en el objeto
 					p.setLenguajeDominante(lenguajeDominante);
 					
-					pro[contPro] = p;
+					// Agrega el objeto "p" a el ArrayList empleados
+					empleados.add(p);
 					
+					// Aumenta contador de programadores
 					contPro++;
 					
 					break;
@@ -119,24 +181,22 @@ public class app {
 			
 			
 			
-		}
-
-		System.out.println("\nLista de empleados\n");
-		for(int i=0; i<contEmp; i++) {
-			System.out.println("Empleado #"+(i+1)+": ");
-			emp[i].imprimir();
-			System.out.print("\n");
-		}
+		}	
 		
-		System.out.println("\nLista de programadores\n");
-		for(int i=0; i<contPro; i++) {
-			System.out.println("Programador #"+(i+1)+": ");
-			pro[i].imprimir();
+		int i = 0;
+		
+		// Imprime los datos de todos los empleados y programadores
+		for(Empleado e: empleados) {
+			System.out.println("Empleado #"+(i+1)+": ");
+			e.imprimir();
 			System.out.print("\n");
+			i++;
 		}
 		
 		// print que se ejecuta si el programa esta por terminar
 		System.out.println("\n\nPROGRAMA FINALIZADO");
+		
 	}
+	
 
 }
